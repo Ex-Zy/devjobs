1f1bf0ed63d066aec95bc9ee778212c7d13565d5
Sergey Nagornykh <serega1905@ukr.net>
2022-06-30 14:15:36 +0300

    refactor jobs service

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,3 +2,5 @@ export const arrayStringToLowerCase = (arr) =>
   Object.values(arr).map((v) => (typeof v === "string" ? v.toLowerCase() : v));
 
 export const lowerCase = (str) => str.toLowerCase();
+
+export const isEmptyString = (str) => str.trim().length === 0;
--- src/services/jobs.service.js
+++ src/services/jobs.service.js
@@ -1,41 +1,59 @@
 import JobsApi from "@api/jobs.api.js";
 import FilterModel from "@models/filter.model.js";
 import { lowerCase, arrayStringToLowerCase } from "@helpers/index.js";
+import { isEmptyString } from "../helpers";
 
-const filterByModelValue = (
-  { keyWord, location, isFulltime } = new FilterModel()
-) => {
-  const CONDITIONS = {
-    ALL: !!(keyWord.length && location.length && isFulltime),
-    KEYWORD_AND_LOCATION: !!(keyWord.length && location.length),
-    KEYWORD_OR_LOCATION: !!(keyWord || location),
-    FULLTIME: isFulltime,
-  };
+// conditions for filtering
+const getFilterConditions = ({ keyWord, location, isFulltime }) => ({
+  ALL: !!(keyWord.length && location.length && isFulltime),
+  KEYWORD_AND_LOCATION: !!(keyWord.length && location.length),
+  KEYWORD_OR_LOCATION: !!(keyWord || location),
+  FULLTIME: isFulltime,
+});
+
+// how to filter specific filter conditions
+const getFilterActions =
+  (values) =>
+  ({ keyWord, location, contract }) => ({
+    ALL: () =>
+      values.includes(lowerCase(keyWord)) &&
+      values.includes(lowerCase(location)) &&
+      values.includes(lowerCase(contract)),
+    KEYWORD_AND_LOCATION: () =>
+      values.includes(lowerCase(keyWord)) &&
+      values.includes(lowerCase(location)),
+    KEYWORD_OR_LOCATION: () =>
+      values.includes(lowerCase(keyWord)) ||
+      values.includes(lowerCase(location)),
+    FULLTIME: () => values.includes(lowerCase(contract)),
+  });
+
+const filterHandler = ({ keyWord, location, isFulltime }) => {
+  const CONDITIONS = getFilterConditions({ keyWord, location, isFulltime });
 
   return (job) => {
-    const values = arrayStringToLowerCase(job);
-    const ACTIONS = {
-      ALL: () =>
-        values.includes(
-          lowerCase(keyWord) && lowerCase(location) && lowerCase("Full Time")
-        ),
-      KEYWORD_AND_LOCATION: () =>
-        values.includes(lowerCase(keyWord) && lowerCase(location)),
-      KEYWORD_OR_LOCATION: () =>
-        values.includes(lowerCase(keyWord) || lowerCase(location)),
-      FULLTIME: () => values.includes(lowerCase("Full Time")),
-    };
-
+    const transformObjValues = arrayStringToLowerCase(job);
+    const ACTIONS = getFilterActions(transformObjValues)({
+      keyWord,
+      location,
+      contract: "Full Time",
+    });
     const active = Object.keys(CONDITIONS).find((key) => CONDITIONS[key]);
 
     return ACTIONS[active]();
   };
 };
 
+const isEmptyFilterModel = ({ keyWord, location, isFulltime }) => {
+  return isEmptyString(keyWord) && isEmptyString(location) && !isFulltime;
+};
+
 export default class JobsService {
   static async filter(model = new FilterModel()) {
+    if (isEmptyFilterModel(model)) return;
+
     const jobs = await JobsApi.getAll();
-    const foundedJobs = jobs.filter(filterByModelValue(model));
+    const foundedJobs = jobs.filter(filterHandler(model));
 
     return foundedJobs;
   }
