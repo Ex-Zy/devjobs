5cd9acf4191393e26387e7c46824b8273283a63d
Sergey Nagornykh <serega1905@ukr.net>
2022-07-05 21:31:45 +0300

    Composable loadmore

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -2,39 +2,18 @@
 import FilterJobs from "@components/FilterJobs.vue";
 import ListJobs from "@jobs/ListJobs.vue";
 import { useJobs } from "@use/useJobs";
-import { ref, watch } from "vue";
+import { useLoadMoreJobs } from "@use/useLoadMoreJobs";
 
 const { jobs } = useJobs();
-
-const visibleJobs = ref([]);
-const showCount = ref(8);
-const step = ref(4);
-
-watch(jobs, () => {
-  updateVisibleJobs(showCount.value);
-});
-
-function updateVisibleJobs(count) {
-  visibleJobs.value = jobs.slice(0, count);
-}
-
-function updateShowCount() {
-  showCount.value = visibleJobs.value.length;
-}
-
-function loadMoreJobs() {
-  const count = visibleJobs.value.length + step.value;
-
-  updateVisibleJobs(count);
-  updateShowCount();
-}
+const { visibleJobs, isVisibleLoadButton, loadMoreJobs } =
+  useLoadMoreJobs(jobs);
 </script>
 
 <template>
   <FilterJobs />
   <LoaderBase v-if="!visibleJobs.length" />
   <ListJobs v-else :jobs="visibleJobs" />
-  <div v-if="showCount < jobs.length" class="load-more">
+  <div v-if="isVisibleLoadButton" class="load-more">
     <ButtonEl title="Load more" @click="loadMoreJobs" class="load-more__btn" />
   </div>
 </template>
