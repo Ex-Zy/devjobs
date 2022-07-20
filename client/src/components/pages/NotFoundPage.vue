97c9c7a3ddb371f0f0a69bef483c6bf256dd971c
Sergey Nagornykh <serega1905@ukr.net>
2022-07-20 13:33:05 +0300

    Add 404 page, build frontend

---
+++ client/src/components/pages/NotFoundPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="page-notfound">404: page not found</h1>
+</template>
+
+<style class="postcss">
+.page-notfound {
+  text-align: center;
+  font-size: 62px;
+  margin-top: 100px;
+}
+</style>
--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,9 +1,12 @@
 import HomePage from "@pages/HomePage.vue";
 import JobPage from "@pages/JobPage.vue";
+import NotFoundPage from "@pages/NotFoundPage.vue";
 
 const routes = [
   { path: "/", component: HomePage },
   { path: "/jobs/:id", component: JobPage },
+  { path: "/404", component: NotFoundPage },
+  { path: "/:pathMatch(.*)*", redirect: "/404" },
 ];
 
 export { routes };
